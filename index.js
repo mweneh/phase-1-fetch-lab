56241f0a2a4d1afc75021712b12d0a80292db7ca
David Ongaro <davidongaroh@gmil.com>
2022-06-14 20:13:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function fetchBooks() {
+ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+    fetch('https://anapioficeandfire.com/api/books')
+   .then(response=>response.json())
+ 
+   .then(jsonData=> {renderBooks(jsonData)  }
+   )}
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -13,5 +17,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks() 
+  
 });
